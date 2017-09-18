20d385f3c70b6f43d6c15629b7565eae4ce5e583
Rossen Totev <rossen.totev@gmail.com>
2017-09-18 16:11:59 -0700

    added the JSDoc documentation for the svyPropertyChange.js

--- svyBase/svyPropertyChange.js
+++ svyBase/svyPropertyChange.js
@@ -1,31 +1,38 @@
 /**
+ * Create a new instance of the PropertyChangeSupport class.
+ * @classdesc This class is a Servoy "wrapper" for the java.beans.PropertyChangeSupport and provides functionality for monitoring and broadcasting property change events to registered listeners.
  * @public
  * @constructor
- * @param {Object} object
+ * @param {Object} object The source object whose property changes are being monitored.
  * @properties={typeid:24,uuid:"6BA8068F-DF21-40F5-9539-DA514E21F26B"}
  */
 function PropertyChangeSupport(object) {
 
     /**
      * Map of property change listeners (for removals)
+     * @ignore
      */
     var map = { };
 
     /**
      * @protected
+     * @ignore
      */
     this.source = object;
 
     /**
-     * @type {java.beans.PropertyChangeSupport}
      * @protected
+     * @type {java.beans.PropertyChangeSupport}
+     * @ignore
      */
     this.propChangeSupport = new java.beans.PropertyChangeSupport(this);
 
     /**
+     * Adds a listener which should be notified whenever the value of a property in the source object is changed.
+     * All registered listeners must be removed using the method {@link PropertyChangeSupport#removeListener}. 
      * @public
-     * @param {Function} listener
-     * @param {String} [propertyName]
+     * @param {Function} listener The callback function to call whenever a property change event is fired. The callback function will receive an input argument object with the following properties: {propetyName: String, oldValue, newValue, source}
+     * @param {String} [propertyName] Optional property name if the listener should be notified only if this specific property is changed. If not specified the listener will be notified when any property in the source object is changed.
      */
     this.addListener = function(listener, propertyName) {
         var method = scopes.svySystem.convertServoyMethodToQualifiedName(listener);
@@ -52,9 +59,10 @@ function PropertyChangeSupport(object) {
     }
 
     /**
+     * Removes a property change listener registered by the {@link PropertyChangeSupport#addListener}.
      * @public
-     * @param {Function} listener
-     * @param {String} [propertyName]
+     * @param {Function} listener The listener callback function to unregister.
+     * @param {String} [propertyName] Optional property name used when the listener function was registered by {@link PropertyChangeSupport#addListener}.
      */
     this.removeListener = function(listener, propertyName) {
         var method = scopes.svySystem.convertServoyMethodToQualifiedName(listener);
@@ -69,8 +77,11 @@ function PropertyChangeSupport(object) {
     }
 
     /**
+     * Use this method to fire a property change event. The event notification will be broadcasted to all applicable registered listeners.
      * @public
-     *
+     * @param {String} propertyName The name of the property which was changed.
+     * @param oldValue The property old value.
+     * @param newValue The property new value.
      */
     this.fireEvent = function(propertyName, oldValue, newValue) {
         this.propChangeSupport.firePropertyChange(propertyName, oldValue, newValue);
