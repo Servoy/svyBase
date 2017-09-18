ff4cad49e5b2368dd684cad828f8290b514d4548
Rossen Totev <rossen.totev@gmail.com>
2017-09-18 15:44:02 -0700

    added the JSDoc documentation for the svyValidationManager.js

--- svyBase/svyValidationManager.js
+++ svyBase/svyValidationManager.js
@@ -63,7 +63,7 @@ function getDataSource(dataSource) {
 }
 
 /**
- * This method validates the specified record combining validation information from all available validation providers for the record's dataSource.
+ * This method validates if the specified record can be saved in the database by combining validation information from all available validation providers for the record's dataSource.
  * Note that the result list may contain not only validation errors but validation markers with information and/or warning levels as well. Use the {@link ValidationMarker#getLevel} to determine the level of the individual validation markers in the result.
  * @public
  * @param {JSRecord} record The record to validate.
@@ -81,9 +81,11 @@ function validate(record){
 }
 
 /**
+ * This method validates if the specified record can be deleted from the database by combining validation information from all available validation providers for the record's dataSource.
+ * Note that the result list may contain not only validation errors but validation markers with information and/or warning levels as well. Use the {@link ValidationMarker#getLevel} to determine the level of the individual validation markers in the result.
  * @public
- * @param {JSRecord} record
- * @return {Array<ValidationMarker>}
+ * @param {JSRecord} record The record to validate.
+ * @return {Array<ValidationMarker>} A list of resulting [validation markers]{@link ValidationMarker} or an empty array if no validation results are available.
  * @properties={typeid:24,uuid:"E8BFC384-ADDA-4AA1-837A-35596662F2E4"}
  */
 function canDelete(record){
@@ -97,63 +99,60 @@ function canDelete(record){
 }
 
 /**
+ * Creates a new instance of the ValidationMarker class.
+ * @classdesc This class encapsulates individual validation results.
  * @public 
- * @param {JSRecord} record
- * @param {String} message
- * @param {String} [level]
- * @param {String} [dataProvider]
  * @constructor 
+ * @param {JSRecord} record The record for which the validation result applies.
+ * @param {String} message The validation result message text.
+ * @param {String} [level] Optional validation result level. If not specified will use {@link VALIDATION_LEVEL#ERROR}.
+ * @param {String} [dataProvider] Optional dataProvider name for which the validation result applies. If provided this usually is a column name.
  * @properties={typeid:24,uuid:"24E0FAA7-5ADF-40BB-9F60-213373546243"}
  */
 function ValidationMarker(record, message, level, dataProvider){
 	
 	/**
+	 * Gets the record for which the validation result applies.
 	 * @public 
-	 * @return {JSRecord}
+	 * @return {JSRecord} The record associated with this ValidationMarker.
 	 */
 	this.getRecord = function(){
 		return record;
 	}
 	
 	/**
+	 * Gets the validation result message text.
 	 * @public 
-	 * @return {String}
+	 * @return {String} The validation result message.
 	 */
 	this.getMessage = function(){
 		return message;
 	}
 	
 	/**
+	 * Gets the datasource of the validation result [record]{@link ValidationMarker#getRecord}.
 	 * @public 
-	 * @return {String}
+	 * @return {String} The datasource of the record or null if a record is not available.
 	 */
 	this.getDataSource = function(){
 		return !record ? null : record.getDataSource();
 	}
 	
 	/**
+	 * Gets the validation result level. This is one of the {@link VALIDATION_LEVEL} enumeration options.
 	 * @public 
-	 * @return {String}
+	 * @return {String} The validation result level.
 	 */
 	this.getLevel = function(){
 		return !level ? VALIDATION_LEVEL.ERROR : level;
 	}
 	
 	/**
+	 * Gets the dataProvider name associated with the validation result.
 	 * @public 
-	 * @return {String}
+	 * @return {String} The dataProvider name, if available.
 	 */
 	this.getDataProvider = function(){
 		return dataProvider;
 	}
-}
-
-/**
- * EXPERIMENTAL
- * @public 
- * @constructor 
- * @properties={typeid:24,uuid:"C09A3F34-FFD4-4C2B-BBA8-D00D82A69FF3"}
- */
-function ValidationEvent(){
-	
 }
